4908f2e650753785f50bb575d8608408a0e0de0a
Shark <rynyaz@gmail.com>
2023-01-09 16:30:12 +0700

    ...

--- JS/external.js
+++ JS/external.js
@@ -1,6 +1,52 @@
-"use strict";
-function checkAge(age) {
-    age < 18 ? "Chua du tuoi" : "Da du tuoi";
+// const arr = [5, 7, 8, 9, 2, 8, 7, 6, 21, 48, 20, 52, 53]
+// function sliceArr(num) {
+//     if (arr.length % 2 == 0) {
+//         return num = arr.length / 2
+//     } if (arr.length % 2 != 0) {
+//         return num = (arr.length - 1) / 2
+//     }
+// }
+// function findOppositeNum(pos) {
+//     if (findPosition() < sliceArr()) {
+//         return pos = sliceArr() + findPosition()
+//     } if (findPosition() > sliceArr()) {
+//         return pos = findPosition() - sliceArr()
+//     } else {
+//         return false
+//     }
+// }
+// let a = findOppositeNum()
+// function findPosition() {
+//     return arr.indexOf(52)
+//     //Nhap code o day
+// }
+// console.log(arr[a])
+
+
+// create function
+// Input hoang
+// out hhoonngg
+function funcCheck(str) {
+    let str2 = ""
+    for (let i = 0; i < str.length; i++) {
+        str2 = str2 + (str[i]) + (str[i])
+    }
+    return str2
 }
-console.log(checkAge(20))
+function checkCondition(str) {
+    if (str != "a") {
+        return true
+    }
+}
+
+console.log(funcCheck("hoang", checkCondition()))
+
+
+
+
+
+
+
+
+
 
